1fc5eaa5572e2a82932b4834de1ff8ba52454a73
strivert <strivert@email.com>
2018-03-14 10:01:25 +0400

    add comment

--- src/components/NameForm.test.js
+++ src/components/NameForm.test.js
@@ -7,6 +7,11 @@ describe('NameForm', () => {
 
   it('renders correctly', () => {
     component = renderer.create(<NameForm />);
+    /*
+    Create a TestRenderer instance with the passed React element.
+    It doesn’t use the real DOM, but it still fully renders the component tree into memory so you can make assertions about it.
+    The returned instance has the following methods and properties.
+    */
   });
 
   it('matches snapshot', () => {
